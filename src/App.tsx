a6e3876397abfc188ced32a6daadd21b8ab7878c
Mohankrishna1998 <mohankrishnagajula19@gmail.com>
2023-05-23 12:07:23 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  
+
+  const [walletAddress, setWalletAddress] = useState("");
+
+  const checkConnection = async () => {
+   
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        if (accounts.length) {
+          console.log(accounts);
+          setWalletAddress(accounts[0]);
+        }
+
+        
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const [fullNavbar, setFullNavbar] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="bg-[#3772FF] text-center text-white font-medium py-[6px] leading-[26px] text-[14px]">
+        Lorem Ipsum is simply dummy text of the printing{" "}
+        {walletAddress.length ? (
+          walletAddress
+        ) : (
+          <button onClick={checkConnection}>connect</button>
+        )}
+      </h1>
+
+      <div className="flex">
+        <Navbar fullNavbar={fullNavbar} setFullNavbar={setFullNavbar} />
+        <Main fullNavbar={fullNavbar} />
+      </div>
+    </>
   );
 }
 
